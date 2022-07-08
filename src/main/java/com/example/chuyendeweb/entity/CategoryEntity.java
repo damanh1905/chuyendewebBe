5e153c8d15b176fb6102355f430cc37dd39ebb93
Hiep299 <85495849+Hiep299@users.noreply.github.com>
2022-07-08 20:54:03 +0700

    update Product Admin

--- src/main/java/com/example/chuyendeweb/entity/CategoryEntity.java
+++ src/main/java/com/example/chuyendeweb/entity/CategoryEntity.java
@@ -1,7 +1,11 @@
 package com.example.chuyendeweb.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,9 +16,10 @@ import java.util.Set;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 @ToString
 @Table(name = "category")
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class CategoryEntity extends BaseEntity {
     @JsonIgnore
